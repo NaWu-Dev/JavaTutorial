a6b02f21261332403cbb61e085f71cb870875a63
NightcatWu <nightcatwu@gmail.com>
2018-10-12 14:19:59 -0400

    Add another class and change configuration.

---
+++ spring/src/com/na/springdemo/ComputerCoach.java
@@ -0,0 +1,10 @@
+package com.na.springdemo;
+
+public class ComputerCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Computer Class will start in Jan. ";
+    }
+
+}
