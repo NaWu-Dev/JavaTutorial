d0190f2c248a5c1a68d10afe2b213744e8525e62
NightcatWu <nightcatwu@gmail.com>
2018-10-14 14:03:42 -0400

    Spring annotation - @Component

---
+++ spring-demo-annotations/src/com/na/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,23 @@
+package com.na.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+    public static void main(String[] arg) {
+
+        // read spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // get the bean from spring container
+        Coach myCoach = context.getBean("thatSillyCoach", Coach.class);
+
+        // call a method on the bean
+        System.out.println(myCoach.getDailyWorkout());
+
+        // close context
+        context.close();
+
+    }
+
+}
