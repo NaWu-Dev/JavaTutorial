3d14d2008498a4d6d78ea56c095fdf0d709c0690
NightcatWu <nightcatwu@gmail.com>
2018-10-13 21:27:23 -0400

    Injection - Literal injection

--- spring/src/com/na/springdemo/CricketCoach.java
+++ spring/src/com/na/springdemo/CricketCoach.java
@@ -4,6 +4,9 @@ public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    // add new fields for emailAddress and team
+    private String emailAddress, team;
+
     // create a no-arg constructor
     public CricketCoach() {
         System.out.println("CricketCoach : inside no-arg constructor");
@@ -13,6 +16,26 @@ public class CricketCoach implements Coach {
         System.out.println("CricketCoach : inside setter method");
         this.fortuneService = fortuneService;
     }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
+    }
+
+
 
     @Override
     public String getDailyWorkout() {
