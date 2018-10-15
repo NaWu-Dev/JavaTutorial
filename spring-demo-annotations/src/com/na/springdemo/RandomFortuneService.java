cbbca2fe09f95728935330eaeeee1eb91d6bf92a
NightcatWu <nightcatwu@gmail.com>
2018-10-15 11:39:49 -0400

    Spring annotation - @qualifier 2

---
+++ spring-demo-annotations/src/com/na/springdemo/RandomFortuneService.java
@@ -0,0 +1,29 @@
+package com.na.springdemo;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+    // create an array of strings
+    private String[] data = {
+            "string one: beware of the wolf",
+            "string two: Diligence is the mother of good luck",
+            "string three: The journey is reward"
+    };
+
+    // create a ramdom number generator
+    private Random myRandom = new Random();
+
+    @Override
+    public String getFortune() {
+        // pick a random string from the array
+        int index = myRandom.nextInt(data.length);
+
+        String theFortune = data[index];
+
+        return theFortune;
+    }
+}
