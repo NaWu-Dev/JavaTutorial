ce4555871bef9ad77897eadaef8b96962d84cdcb
NightcatWu <nightcatwu@gmail.com>
2018-11-09 14:35:02 -0500

    Spring security - demo configuration without xml

---
+++ spring-security-demo-01-base-app/src/main/java/com/nana/springsecurity/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.nana.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String showHome() {
+
+        return "home";
+
+    }
+
+}
