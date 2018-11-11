424f95d2c4ea1f54c4f9f3f2c980b70d475a5f51
NightcatWu <nightcatwu@gmail.com>
2018-11-11 06:14:43 -0500

    Spring REST - PathVariable

--- spring-rest-demo/src/main/java/com/nana/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/nana/springdemo/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.nana.springdemo.rest;
 
 import com.nana.springdemo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,18 +14,34 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students"
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
+    // define @PostConstruct to load the student data.. only once
+    @PostConstruct
+    private void loadData() {
+
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Marry", "Smith"));
         theStudents.add(new Student("Na", "Wu"));
 
+    }
+
+    // define endpoint for "/students"
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
         return theStudents;
+
+    }
+
+    // define endpoint for "/students/{studentid}" return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        return theStudents.get(studentId);
 
     }
 
--- spring-rest-demo/src/main/webapp/index.jsp
+++ spring-rest-demo/src/main/webapp/index.jsp
@@ -1,7 +1,13 @@
 <html>
 <body>
 
-Spring REST Demo
+<h3>Spring REST Demo</h3>
+
+<hr>
+
+<a href="${pageContext.request.contextPath}/test/hello">HELLO</a>
+
+<a href="${pageContext.request.contextPath}/api/students">Get all Students</a>
 
 
 </body>
