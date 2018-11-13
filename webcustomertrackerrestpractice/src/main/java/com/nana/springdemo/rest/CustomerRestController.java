14753f00a5f90fdc02c09979c498a0121aac1075
NightcatWu <nightcatwu@gmail.com>
2018-11-13 09:13:08 -0500

    Spring Rest - Practice - Customer system list customers

---
+++ webcustomertrackerrestpractice/src/main/java/com/nana/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,26 @@
+package com.nana.springdemo.rest;
+
+import com.nana.springdemo.entity.Customer;
+import com.nana.springdemo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers() {
+
+        return customerService.listCustomers();
+
+    }
+
+}
