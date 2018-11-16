07a76712a7610beb794e91fae9b5024466f85bf2
NightcatWu <nightcatwu@gmail.com>
2018-11-15 21:49:16 -0500

    Spring boot - Location web sys - Handle create request and send a request back

--- springdatarest/locationweb/src/main/java/com/nana/location/controller/LocationController.java
+++ springdatarest/locationweb/src/main/java/com/nana/location/controller/LocationController.java
@@ -1,13 +1,30 @@
 package com.nana.location.controller;
 
+import com.nana.location.entity.Location;
+import com.nana.location.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LocationController {
 
+    @Autowired
+    private LocationService locationService;
+
     @RequestMapping("/showCreate")
     public String showCreate() {
+        return "createLocation";
+    }
+
+    @PostMapping("/saveLoc")
+    public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap) {
+        Location locationSaved = locationService.saveLocation(location);
+        String msg = "Location saved with id: " + locationSaved.getId();
+        modelMap.addAttribute("msg", msg);
         return "createLocation";
     }
 
