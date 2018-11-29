a1cc72503d2d42cf7ae666ea9f4ba18d8c14f57a
NightcatWu <nightcatwu@gmail.com>
2018-11-29 10:31:40 -0500

    Configuration tutorial - Configure profile specific bean

--- tutorial-configuration/src/main/java/com/nana/practice/tutorialconfiguration/TutorialConfigurationApplication.java
+++ tutorial-configuration/src/main/java/com/nana/practice/tutorialconfiguration/TutorialConfigurationApplication.java
@@ -2,11 +2,34 @@ package com.nana.practice.tutorialconfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class TutorialConfigurationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TutorialConfigurationApplication.class, args);
+
+        ApplicationContext applicationContext =
+                SpringApplication.run(TutorialConfigurationApplication.class, args);
+
+        for (String name : applicationContext.getBeanDefinitionNames())
+            System.out.println(name);
+
     }
+
+    @Profile("dev")
+    @Bean
+    public String devBean(){return "dev";};
+
+    @Profile("test")
+    @Bean
+    public String testBean(){return "test";};
+
+    @Profile("prod")
+    @Bean
+    public String prodBean(){return "prod";};
+
+
 }
