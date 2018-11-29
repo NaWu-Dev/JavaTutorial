fb7420fbc6c7529e63f619a60ff41bdaa7daa800
NightcatWu <nightcatwu@gmail.com>
2018-11-29 09:57:01 -0500

    Configuration tutorial - init project

---
+++ tutorial-configuration/src/main/java/com/nana/practice/tutorialconfiguration/TutorialConfigurationApplication.java
@@ -0,0 +1,12 @@
+package com.nana.practice.tutorialconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialConfigurationApplication.class, args);
+    }
+}
