9c5e7c40235c70eadb9bc0f310bae19921c35b85
NightcatWu <nightcatwu@gmail.com>
2018-11-29 10:18:38 -0500

    Configuration tutorial - Add configuration property

--- tutorial-configuration/src/main/java/com/nana/practice/tutorialconfiguration/WelcomeResource.java
+++ tutorial-configuration/src/main/java/com/nana/practice/tutorialconfiguration/WelcomeResource.java
@@ -1,8 +1,13 @@
 package com.nana.practice.tutorialconfiguration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WelcomeResource {
@@ -13,6 +18,19 @@ public class WelcomeResource {
     @GetMapping("/welcome")
     public String retrieveWelcomeMessage() {
         return welcomeMessage;
+    }
+
+    @Autowired
+    private BasicConfiguration configuration;
+
+    @RequestMapping("/dynamic-configuration")
+    public Map<String, Object> dynamicConfiguration() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("message", configuration.getMessage());
+        map.put("number", configuration.getNumber());
+        map.put("key", configuration.isValue());
+        return map;
+
     }
 
 }
